d021e63698416c9422b2dead6e6d0035ffd72567
Umair452 <arshadu452@gmail.com>
2024-08-23 00:21:27 +0500

    how react creates tree and elements

--- react-code/src/main.jsx
+++ react-code/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// index.js or App.js
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
+const reactElement = React.createElement(
+  'h1',
+  { className: 'myHeading', id: 'headingId' },
+  'I am the injected heading'
+);
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById('root'));
+root.render(
+  reactElement
+);
